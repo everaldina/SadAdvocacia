df702edce847174a65879c9766821fc59725893f
Everaldina G. Barbosa <everaldinag@gmail.com>
2024-03-15 16:23:17 -0300

    add rotas

--- sad_app/views.py
+++ sad_app/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from sad_app.models import Membro
 
 # Create your views here.
+def home(request):
+    equipe = Membro.objects.order_by('id')
+    context = {
+        'equipe': equipe
+    }
+    return render(request, 'index.html', context)
