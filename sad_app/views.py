38c0d52a30519b712711ee68887a2b005c897d25
Everaldina G. Barbosa <everaldinag@gmail.com>
2024-04-10 23:37:08 -0300

    att equipe display

--- sad_app/views.py
+++ sad_app/views.py
@@ -5,17 +5,21 @@ from sad_app.models import tipoPublicacao
 
 # Create your views here.
 def home(request):
-    equipe = Membro.objects.order_by('id')
-    consultor = []
-    advogado = []
-    for e in equipe:
-        if e.cargo == 'Consultor':
-            consultor.append(e)
-        else:
-            advogado.append(e)
+    cargos = [cargo[0] for cargo in Membro.Cargo.choices]
+    equipe = Membro.objects.all()
+    equipe_por_cargo = {}
+    for cargo in cargos:
+        membros = equipe.filter(cargo=cargo)
+        nome_cargo = dict(Membro.Cargo.choices).get(cargo)
+        equipe_por_cargo[nome_cargo] = membros
+        
+    for cargo in equipe_por_cargo:
+        for membro in equipe_por_cargo[cargo]:
+            membro.formacao_ordenadas = membro.formacao_set.all().order_by('ano_entrada')
+            
+    
     context = {
-        'consultores': consultor,
-        'advogados': advogado,
+        'equipe_por_cargo': equipe_por_cargo
     }
     return render(request, 'index.html', context)
 
